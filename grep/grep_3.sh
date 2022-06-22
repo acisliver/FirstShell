b60e4dc52c588090dccb3480daca2daaac67e0a8
acisliver <7273322@naver.com>
2022-06-22 23:03:43 +0900

    grep -G

--- grep/grep_3.sh
+++ grep/grep_3.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # -E
+# 확장 정규 표현식에 해당하는 패턴을 검색할 경우
 grep 'q[[:lower:]]*\??' expression.txt
 grep -E 'q[[:lower:]]*\??' expression.txt
 
 # -F
+# 패턴이 여러 줄일 경우
 grep -F '# Date
 # Author
 # Description' expression.txt
+
+# -G
+# 기본 정규 표현식 사용(기본값)
+grep -G '[[:lower:]]*\??' expression.txt
