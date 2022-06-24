9dc1998cbb1e8e0248f4b44e104020d034f9f9fb
acisliver <7273322@naver.com>
2022-06-24 14:47:05 +0900

    grep -f

--- grep/grep_4.sh
+++ grep/grep_4.sh
@@ -4,3 +4,9 @@
 # -e 패턴, --regexp= 패턴 옵션
 # 특정 단어가 포함된 라인 검색
 grep -e 'mail' --regexp 'phone' expression.txt
+
+# -f 파일, --file= 파일 옵션
+# 패턴을 파일로 저장 후 사용
+echo 'mail' > file1.txt
+echo 'phone' > file2.txt
+grep -f file1.txt --file=file2.txt expression.txt
