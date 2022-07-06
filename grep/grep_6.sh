8d6501826d6eaac00de82a062fe7cac25fa6ca49
acisliver <7273322@naver.com>
2022-07-06 22:46:40 +0900

    ls -l | grep -label=LABEL

--- grep/grep_6.sh
+++ grep/grep_6.sh
@@ -15,3 +15,8 @@ grep -H 'express' expression.txt
 # -h, --no-filename
 # 반대로 전체파일을 대상으로 검색하여도 파일명을 보여주지 않음
 grep -h 'express' ./*
+
+# --label=LABEL
+# 검색한 파일 정보 앞에 라벨을 붙여줄 경우
+# 검색 대상 자체가 파일이여야 해서 ls와 파이프라인 사용
+ls -l | grep --label=file -H express
