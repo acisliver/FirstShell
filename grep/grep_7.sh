b51641284bd42c04f9c08e681c4c88f41ab7eceb
acisliver <7273322@naver.com>
2022-07-12 22:28:10 +0900

    grep -C

--- grep/grep_7.sh
+++ grep/grep_7.sh
@@ -10,3 +10,7 @@ grep -A 2 'question' expression.txt
 # -B 라인 수, --before-context=라인수
 # 검색한 패턴이 있는 라인을 기준으로 라인 위를 보여줌
 grep -B 2 'question' expression.txt
+
+# -C 라인 수, - 라인수, --context=라인 수
+# 검색한 패턴이 있는 라인을 기준으로 라인 위, 아래를 보여줌
+grep -C 2 'question' expression.txt
