32d638bb39d82d3aa1aa441aac09950f176fa1fc
acisliver <7273322@naver.com>
2022-07-14 23:30:49 +0900

    grep --group-separator

--- grep/grep_7.sh
+++ grep/grep_7.sh
@@ -14,3 +14,8 @@ grep -B 2 'question' expression.txt
 # -C 라인 수, - 라인수, --context=라인 수
 # 검색한 패턴이 있는 라인을 기준으로 라인 위, 아래를 보여줌
 grep -C 2 'question' expression.txt
+
+# --group-separator=그룹구분 기호
+# A, B, C 옵션과 함께 사용
+# 여러 섹션에서 패턴이 검색되었을 때 구분자로 사용
+grep -A 1 --group-separator="=======" '# [[:alpha:]]' expression.txt
