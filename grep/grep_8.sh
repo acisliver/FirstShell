d19c054fe89c52fd15a6aca92935a3a77f95656d
Daeeun Ko <7273322@naver.com>
2022-07-18 23:08:39 +0900

    grep --binary-files=TYPE

--- grep/grep_8.sh
+++ grep/grep_8.sh
@@ -6,3 +6,10 @@
 # -a, --text
 # 패턴의 검색 대상이 바이너리 파일인 경우 바이너리 파일을 텍스트로 간주하고 패턴 검색
 grep -a "^Content" grep_binary_test
+
+# --binary-files=TYPE
+# TYPE=binary, text, without-match
+# 바이너리 파일 타입이 binary일 때 파일에 매칭되는 패턴이 있다는 메시지를 보여줌
+grep --binary-files=binary '^Context' grep_binary_test
+# 바이너리 파일이 text일 때 패턴이 포함된 라인을 보여줌
+grep --binary-files=text '^Context' grep_binary_test
